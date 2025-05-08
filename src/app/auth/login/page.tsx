a79eee0a39bd5aadbebc0c171ffdd210ebbf6da7
FT39968555 <marcosgaliano96@gmail.com>
2025-05-08 01:53:19 -0300

    fix tipado

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -8,97 +8,103 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function AuthPage() {
-  
   const [isLogin, setIsLogin] = useState(true);
   const [form, setForm] = useState({ email: "", password: "" });
   const [isLoading, setIsLoading] = useState(false);
-  
-  const getTextButton = () : string =>{
-    if(isLoading) return 'Cargando...'
-    if(isLogin) return 'Ingresar'
-    else return 'Crear cuenta'
-  }
 
-  if(isLoading) return <LoadingOverlay text={isLogin ? "Obteniendo datos..." : "Registrando usuario..."} />
+  const getTextButton = (): string => {
+    if (isLoading) return "Cargando...";
+    if (isLogin) return "Ingresar";
+    else return "Crear cuenta";
+  };
+
+  if (isLoading)
+    return (
+      <LoadingOverlay
+        text={isLogin ? "Obteniendo datos..." : "Registrando usuario..."}
+      />
+    );
   return (
     <div className="max-w-md !mx-auto  p-6 h-[70vh]  !my-16">
       <div className="h-fit">
+        <h2 className="text-2xl font-bold !mb-4 text-center">
+          {isLogin ? "Iniciar sesión" : "Registrarse"}
+        </h2>
 
-      <h2 className="text-2xl font-bold !mb-4 text-center">
-        {isLogin ? "Iniciar sesión" : "Registrarse"}
-      </h2>
+        <form
+          action={async (formData) => {
+            setIsLoading(true);
+            const email = formData.get("email") as string;
+            const password = formData.get("password") as string;
 
-      <form
- action={async (formData) => {
-  setIsLoading(true);
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
+            try {
+              if (!isLogin) {
+                const response = await registerUser({ email, password });
 
-  try {
-    if (!isLogin) {
-      const response = await registerUser({ email, password });
+                if (response?.error) {
+                  toast.error(response.error.message);
+                  setIsLoading(false);
+                  return;
+                }
 
-      if (response?.error) {
-        toast.error(response.error.message);
-        setIsLoading(false);
-        return;
-      }
+                toast.success("Usuario creado con éxito");
+                setIsLogin(true);
+                setIsLoading(false);
+              } else {
+                const res = await signIn("credentials", {
+                  email,
+                  password,
+                  redirect: false,
+                });
 
-      toast.success("Usuario creado con éxito");
-      setIsLogin(true);
-      setIsLoading(false);
-    } else {
-      const res = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-      });
-
-      if (res?.error) {
-        toast.error("Usuario o contraseña incorrectos");
-        setIsLoading(false);
-      } else {
-        const redirectUrl = new URLSearchParams(window.location.search).get("redirect_uri") || "/";
-        window.location.href = redirectUrl;
-      }
-    }
-  } catch (err: any) {
-    toast.error("Ocurrió un error inesperado");
-    console.error("Error en autenticación:", err);
-    setIsLoading(false);
-  }
-}}
-        className="space-y-4"
-      >
-        <TextField
-          type="email"
-          placeholder="Email"
-          value={form.email}
-          name="email"
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-          // className="w-full p-2 border rounded"
-          required
-        />
-        <TextField
-          type="password"
-          placeholder="Contraseña"
-          name="password"
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-          // className="w-full p-2 border rounded"
-          required
-        />
-
-        <button
-          type="submit"
-          className="w-full bg-red-600 text-white !p-2 rounded-2xl hover:bg-red-700 cursor-pointer"
-          disabled={isLoading}
+                if (res?.error) {
+                  toast.error("Usuario o contraseña incorrectos");
+                  setIsLoading(false);
+                } else {
+                  const redirectUrl =
+                    new URLSearchParams(window.location.search).get(
+                      "redirect_uri"
+                    ) || "/";
+                  window.location.href = redirectUrl;
+                }
+              }
+            } catch (err) {
+              toast.error("Ocurrió un error inesperado");
+              console.error("Error en autenticación:", err);
+              setIsLoading(false);
+            }
+          }}
+          className="space-y-4"
         >
-          {getTextButton()}
-        </button>
-      </form>
+          <TextField
+            type="email"
+            placeholder="Email"
+            value={form.email}
+            name="email"
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            // className="w-full p-2 border rounded"
+            required
+          />
+          <TextField
+            type="password"
+            placeholder="Contraseña"
+            name="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            // className="w-full p-2 border rounded"
+            required
+          />
 
-      {/* <div className="my-4 text-center">o</div>
+          <button
+            type="submit"
+            className="w-full bg-red-600 text-white !p-2 rounded-2xl hover:bg-red-700 cursor-pointer"
+            disabled={isLoading}
+          >
+            {getTextButton()}
+          </button>
+        </form>
+
+        {/* <div className="my-4 text-center">o</div>
 
       <button
         onClick={() =>
@@ -111,15 +117,16 @@ export default function AuthPage() {
         Iniciar con Google
       </button> */}
 
-      <p className="!mt-4 text-sm text-center">
-        {isLogin ? "¿No tenés cuenta?" : "¿Ya tenés cuenta?"}{"  "}
-        <button
-          onClick={() => setIsLogin(!isLogin)}
-          className="text-red-600 underline cursor-pointer"
-        >
-          {isLogin ? "Registrate" : "Iniciar sesión"}
-        </button>
-      </p>
+        <p className="!mt-4 text-sm text-center">
+          {isLogin ? "¿No tenés cuenta?" : "¿Ya tenés cuenta?"}
+          {"  "}
+          <button
+            onClick={() => setIsLogin(!isLogin)}
+            className="text-red-600 underline cursor-pointer"
+          >
+            {isLogin ? "Registrate" : "Iniciar sesión"}
+          </button>
+        </p>
       </div>
     </div>
   );
