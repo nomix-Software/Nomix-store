84db980901dae1269c4a7334603da91c75753382
FT39968555 <marcosgaliano96@gmail.com>
2025-05-22 10:06:09 -0300

    fix para ambiente

--- src/app/(shop)/checkout/success/page.tsx
+++ src/app/(shop)/checkout/success/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import { FaCheckCircle } from "react-icons/fa";
 
 const Success = () => {
-  const searchParams = useSearchParams();
   const [loading, setLoading] = useState(true);
   const [ventaRegistrada, setVentaRegistrada] = useState(false);
 
   useEffect(() => {
     const registrarVenta = async () => {
+      const searchParams = new URLSearchParams(window.location.search)
       const paymentId = searchParams.get("payment_id");
       const status = searchParams.get("status");
       const preferenceId = searchParams.get("preference_id");
@@ -43,7 +42,7 @@ const Success = () => {
     };
 
     registrarVenta();
-  }, [searchParams]);
+  }, []);
 
   return (
     <div className="min-h-screen flex items-center justify-center !px-4">
