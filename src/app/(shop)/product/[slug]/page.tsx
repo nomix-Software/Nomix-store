7613a3567bd3d0b190fb2e9db26806fbe27709d9
FT39968555 <marcosgaliano96@gmail.com>
2025-06-03 14:07:52 -0300

    agrego bloqueo de stock en detalle

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 export const revalidate = 60; // en segundos
 import React from "react";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
@@ -9,15 +8,16 @@ import { getProductDetail, getProducts } from "@/actions";
 import { notFound } from "next/navigation";
 
 import { AddToCart, ImagesDetails, RelatedProducts } from "@/components";
+import { MdErrorOutline } from "react-icons/md";
 interface Props {
-  params : Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }
 export async function generateStaticParams() {
   const productos = await getProducts(); // o fetch a tu DB
   return productos.map((p) => ({ slug: p.slug.current }));
 }
 const ProductDetails = async ({ params }: Props) => {
-  const { slug } = await params
+  const { slug } = await params;
   const productDetail: ProductDetails | null = await getProductDetail(slug);
   if (!productDetail) notFound();
 
@@ -40,11 +40,25 @@ const ProductDetails = async ({ params }: Props) => {
           </div>
           <h4 className="font-bold">Detalle: </h4>
           <p className="whitespace-pre-line">{productDetail.descripcion}</p>
-          <AddToCart  {...productDetail} imagenURI={productDetail.imagenes[0].url} />
+          {productDetail.stock <= 0 ? (
+            <div className="inline-flex items-center gap-2 bg-red-200 text-red-800 text-base font-semibold !px-4 !py-1.5 !mt-6 mx-auto rounded-full !mb-4 shadow-sm">
+              <MdErrorOutline className="text-xl" />
+              Sin stock disponible
+            </div>
+          ): 
+          <AddToCart
+            {...productDetail}
+            imagenURI={productDetail.imagenes[0].url}
+          />
+          }
         </div>
       </div>
       {/* tambien te puede gustar */}
-      <RelatedProducts productId={productDetail.id} categoriaId={productDetail.categoria.id} marcaId={productDetail.marca.id} />
+      <RelatedProducts
+        productId={productDetail.id}
+        categoriaId={productDetail.categoria.id}
+        marcaId={productDetail.marca.id}
+      />
     </div>
   );
 };
