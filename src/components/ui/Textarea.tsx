fb6b91722995bef380a90a50d0d120a2371673e3
FT39968555 <marcosgaliano96@gmail.com>
2025-06-25 17:16:06 -0300

    altura ajustable de textarea

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -26,8 +26,9 @@ export default function Textarea({
         name={name}
         value={value}
         onChange={onChange}
-        className={`w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 ${className}`}
+        className={`w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 resize-y ${className}`}
         placeholder={placeholder}
+        
       />
       <p className="text-xs text-gray-500 mt-1 !px-4">{helperText}</p>
     </div>
