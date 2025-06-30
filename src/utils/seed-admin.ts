65a3d228d846a80759ddd20a11e135dc617a63d9
FT39968555 <marcosgaliano96@gmail.com>
2025-06-30 16:25:35 -0300

    scripts utiles

--- src/utils/seed-admin.ts
+++ src/utils/seed-admin.ts
@@ -6,11 +6,33 @@ async function main() {
   // LIMPIAR BASE DE DATOS (orden correcto)
 
   // Crear usuario
- await prisma.user.update({ 
-    where:{ email : 'marcosgaliano96@gmail.com'}, data: { rol : 'ADMIN'}
+//  await prisma.user.update({ 
+//     where:{ email : 'marcosgaliano96@gmail.com'}, data: { rol : 'ADMIN'}
 
-  });
+//   });
+const product = await prisma.producto.findFirst({ where:{ slug: 'pizarra-capibara'} })
 
+console.log({product})
+  await prisma.imagenProducto.deleteMany({
+  where: { productoId: 36 },
+});
+
+await prisma.carritoItem.deleteMany({
+  where: { productoId: 36 },
+});
+
+await prisma.ventaProducto.deleteMany({
+  where: { productoId: 36 },
+});
+
+await prisma.favorito.deleteMany({
+  where: { productoId: 36 },
+});
+
+// Finalmente, borrás el producto
+await prisma.producto.delete({
+  where: { id: 36 },
+});
   
   console.log("Seed limpio y cargado con productos electrónicos.");
 }
