7763d763e107f4b01bbbe667f490fa880accc7a2
FT39968555 <marcosgaliano96@gmail.com>
2025-07-14 23:53:14 -0300

    menos importaciones en desuso

--- src/app/(shop)/dashboard/product/edit/[slug]/page.tsx
+++ src/app/(shop)/dashboard/product/edit/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { FiSave, FiTrash2 } from "react-icons/fi";
+import {  FiTrash2 } from "react-icons/fi";
 import {
   getProductDetail,
   getCategories,
