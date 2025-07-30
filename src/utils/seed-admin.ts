c5fe73572815d49df8cfac42caca558c78a37e7e
FT39968555 <marcosgaliano96@gmail.com>
2025-07-30 17:02:10 -0300

    eliminar usuarios

--- src/utils/seed-admin.ts
+++ src/utils/seed-admin.ts
@@ -4,8 +4,12 @@ import { sendCouponWonEmail } from "../actions/sendEmail/sendCouponWonEmail";
 const prisma = new PrismaClient();
 
 async function main() {
-  await sendCouponWonEmail("Marcosgaliano96@gmail.com", "CUPON123", 20);
-  console.log("Correo de prueba enviado");
+  const usuario = await prisma.user.delete({
+    where: { email: "cliente1@cliente.com" },
+
+  });
+  const usuarios = await prisma.user.findMany()
+  console.log("Usuario actualizado a ADMIN", { usuarios, usuario });
 }
 
 main()
